767df6f45eb42504c10180cb524c4f86f1f051d9
Núria Murgui <correunuriam@gmail.com>
2018-06-02 13:37:04 +0200

    Html y JavaScript comentados

--- src/components/enviar.php
+++ src/components/enviar.php
@@ -1,3 +1,7 @@
+<!--Documento php para enviar el formulario a un correo. No hemos conseguido 
+que funcione pero al eliminarlo gulp nos daba problemas, por lo que hemos 
+decidido dejarlo aunque no sea funcional.
+En la carpeta dist aparece como un archivo html -->
 
 <?php
 
